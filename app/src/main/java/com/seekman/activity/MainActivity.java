9a444aaeaebed272bf17ba3d8796cae6baf3f482
fromUser <838437017@qq.com>
2016-05-17 00:08:28 +0800

    项目初始结构，统一架构，统一命名规范

---
+++ app/src/main/java/com/seekman/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.seekman.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seekman.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
