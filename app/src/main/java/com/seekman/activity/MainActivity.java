89293fb74c8b3bfc331e51bd3791ae778245b65a
fromUser <838437017@qq.com>
2016-05-18 20:37:27 +0800

    按钮点击事件的回调函数的声明。

--- app/src/main/java/com/seekman/activity/MainActivity.java
+++ app/src/main/java/com/seekman/activity/MainActivity.java
@@ -2,14 +2,22 @@ package com.seekman.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.seekman.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+    }
+
+    public void register(View view){
+
+        Log.i (TAG, "register: --------注册----------");
     }
 }
