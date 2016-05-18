677e6bfa7d465d190b5fbbc92301c0801ee42233
fromUser <838437017@qq.com>
2016-05-18 21:15:42 +0800

    第三方验证库mob码，集成

--- app/src/main/java/com/seekman/activity/MainActivity.java
+++ app/src/main/java/com/seekman/activity/MainActivity.java
@@ -1,11 +1,12 @@
 package com.seekman.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
 import com.seekman.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
